91b1ef53b5e1eb0d2bc6637e796200ad980b861e
Simo0o08 <48889237+Simo0o08@users.noreply.github.com>
2020-08-29 17:30:17 +0530

    Add files via upload

---
+++ src/SimpleModal.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+// import HomePage from './HomePage';
+
+
+
+// const useStyles = makeStyles((theme) => ({
+  
+// }));
+
+export default function SimpleModal() {
+  // const classes = useStyles();
+  // getModalStyle is not a pure function, we roll the style only on the first render
+  
+  return (
+    <div>
+      {/* <button type="button" onClick={handleOpen}>
+        Open Modal
+      </button> */}
+      
+      
+    </div>
+  );
+}
